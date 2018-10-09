d2ab89608f904c19b0b4f83ede75fc5ff0317c7d
Uddhav Bhosle <ubhosle@ncsu.edu>
2018-10-09 18:45:45 -0400

    fix pointer issues, reference correct container List

--- kernel_module/src/ioctl.c
+++ kernel_module/src/ioctl.c
@@ -60,25 +60,37 @@ struct container {
     struct container *next;
 };
 
-struct container *containerList;
+struct container *containerList =  NULL;
 
-// memory allocation
-void *doMalloc (size_t sz) {
-    void *mem = kmalloc(sz, GFP_KERNEL);
-    if (mem == NULL) {
-        printk ("No memory to allocate\n");
-        return NULL;
-    }
-    return mem;
-}
+// // memory allocation
+// void *doMalloc (size_t sz) {
+//     void *mem = kmalloc(sz, GFP_KERNEL);
+//     if (mem == NULL) {
+//         printk ("No memory to allocate\n");
+//         return NULL;
+//     }
+//     return mem;
+// }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // add thread to the end of th thread list
-void addThread (struct thread *first, int new_tid) {
+void addThread (int curr_cid, int new_tid) {
     struct thread *curr=NULL;
-    struct thread *newNode = NULL;
+    struct thread *first = NULL;
+    // struct thread *newNode = NULL;
+    struct container * currContainer = containerList;
+    while(currContainer != NULL)
+    {
+      if(currContainer->cid == curr_cid)
+      {
+        first = currContainer->headThread;
+        break;
+      }
+      currContainer = currContainer->next;
+    }
     printk("\t\tstart addThread\n");
-    newNode = doMalloc(sizeof (struct thread));
+    struct thread* newNode = (struct thread*) kcalloc(1, sizeof(struct thread), GFP_KERNEL);
+    // newNode = doMalloc(sizeof (struct thread));
     newNode->current_thread = current;
     newNode->tid = new_tid;
     newNode->next = NULL;
@@ -115,30 +127,47 @@ void addThread (struct thread *first, int new_tid) {
 }
 
 // remove thread from given container
-void removeTopThread (struct container *head, int curr_cid) {
+void removeTopThread (int curr_cid) {
     struct container *curr = NULL;
     struct container *prev = NULL;
     printk("\t\tstart removeTopThread\n");
-    curr = head;
+    curr = containerList;
     while(curr!=NULL)
     {
+      printk("in while loop\n");
       if(curr->cid == curr_cid)
       {
+        printk("in if curr->cid == curr_cid\n");
         struct thread *first = curr->headThread;
-        if(first->next == NULL)
+        printk("variable->'first' assigned \n");
+        if(first == NULL)
         {
-          kfree(first);
-          first=NULL;
-          if(prev == NULL) prev = curr->next;
+          printk("container is empty!!\n");
+          if(prev == NULL) containerList = curr->next;
           else prev->next = curr->next;
+          printk("free container mem\n");
           kfree(curr);
           curr=NULL;
           mutex_unlock(&lock);
-          head = prev;
+          return;
+        }
+        if(first->next == NULL)
+        {
+          printk("only thread in container list\n");
+          printk("free thread mem\n");
+          kfree(first);
+          first=NULL;
+          if(prev == NULL) containerList = curr->next;
+          else prev->next = curr->next;
+          printk("free container mem\n");
+          kfree(curr);
+          curr=NULL;
+          mutex_unlock(&lock);
           return;
         }
         curr->headThread = first->next;
-        wake_up_process(first->next->current_thread);
+        printk("wake up next process\n");
+        wake_up_process(curr->headThread->current_thread);
         kfree(first);
         first=NULL;
         return;
@@ -152,23 +181,29 @@ void removeTopThread (struct container *head, int curr_cid) {
 }
 
 // add container to the end of the container list
-void addContainer (struct container *head, int new_cid, int new_tid) {
+void addContainer (int new_cid, int new_tid) {
    struct container *curr=NULL;
-   struct container *new_node = NULL;
+   // struct container *new_node = NULL;
    printk("\t\tstart addContainer\n");
-   new_node = doMalloc(sizeof (struct container));
+   struct container* new_node = (struct container*) kcalloc(1, sizeof(struct container), GFP_KERNEL);
+   // new_node = doMalloc(sizeof (struct container));
    new_node->cid  = new_cid;
    new_node->headThread = NULL;
    new_node->next = NULL;
-   if(head == NULL)
+   if(containerList == NULL)
      {
          printk("head is null\n");
-         head=new_node;
-         addThread(head->headThread, new_tid);
+         containerList=new_node;
+         containerList->headThread = (struct thread*) kcalloc(1, sizeof(struct thread), GFP_KERNEL);
+         // head->headThread = doMalloc(sizeof (struct thread));
+         containerList->headThread->current_thread = current;
+         containerList->headThread->tid = new_tid;
+         containerList->headThread->next = NULL;
+         mutex_unlock(&lock);
          return;
      }
   else {
-    curr = head;
+    curr = containerList;
     while(curr->next!=NULL)
     {
       curr = curr->next;
@@ -176,32 +211,33 @@ void addContainer (struct container *head, int new_cid, int new_tid) {
     curr->next=new_node;
   }
   printk("\t\tend addContainer\n");
-  addThread(curr->next->headThread, new_tid);
+  addThread(curr->next->cid, new_tid);
 }
 
 
 // check if container exists or add it if it does not
-void addToContainer(struct container *head, int new_cid, int new_tid)
+void addToContainer(int new_cid, int new_tid)
 {
-  struct container *curr=head;
+
+  struct container *curr=containerList;
   printk("\t\tstart addTOContainer\n");
   printk("Add to container:\tCID: %d\tTID: %d\n",new_cid,new_tid);
-  if(head == NULL)
+  if(containerList == NULL)
     {
         printk("head is null in addTOcontainer\n");
-        addContainer(head, new_cid, new_tid);
+        addContainer(new_cid, new_tid);
         return;
     }
   while(curr!=NULL)
   {
     if(curr->cid == new_cid)
     {
-      addThread(curr->headThread, new_tid);
+      addThread(curr->cid, new_tid);
       return;
     }
     curr=curr->next;
   }
-  addContainer(head, new_cid, new_tid);
+  addContainer(new_cid, new_tid);
   printk("\t\tend addTOContainer\n");
   return;
 }
@@ -239,11 +275,11 @@ void printMap(struct container *head)
 }
 
 // remove current thread and add it to the end of queue
-void addCurrentThreadAtEnd (struct container *head,int cid) {
+void addCurrentThreadAtEnd (int cid) {
     struct container *currContainer=NULL;
     struct thread *first=NULL;
     printk("\t\tstart addCurrentThreadAtEnd\n");
-    currContainer=head;
+    currContainer=containerList;
     printk("add current thread at end:\tCID: %d\n",cid);
     //find the container for cid
     while(currContainer!=NULL)
@@ -291,11 +327,11 @@ void addCurrentThreadAtEnd (struct container *head,int cid) {
    }
 }
 
-int findContainerForThread(struct container *head, int curr_tid)
+int findContainerForThread(int curr_tid)
 {
   struct container *curr_container=NULL;
   printk("\t\tstart findContainerForThread\n");
-  curr_container = head;
+  curr_container = containerList;
   printk("find container for thread:\tTID: %d\n",curr_tid);
   while(curr_container != NULL)
   {
@@ -322,13 +358,15 @@ int findContainerForThread(struct container *head, int curr_tid)
 int processor_container_delete(struct processor_container_cmd __user *user_cmd)
 {
     int curr_cid;
-    struct processor_container_cmd userInfo;
+    struct processor_container_cmd* userInfo = (struct processor_container_cmd*) kcalloc(1, sizeof(struct processor_container_cmd), GFP_KERNEL);
     mutex_lock(&lock);
     printk("\t\tprocessor_container_delete ****************************\n");
-    copy_from_user(&userInfo,user_cmd,sizeof(userInfo));
-    curr_cid = (int)userInfo.cid;
+    copy_from_user(userInfo, user_cmd, sizeof(struct processor_container_cmd));
+    curr_cid = (int)userInfo->cid;
+    kfree(userInfo);
+    userInfo = NULL;
     printk("deleting container CID:\t%d\n", curr_cid);
-    removeTopThread(containerList,curr_cid);
+    removeTopThread(curr_cid);
     // printMap(containerList);
     mutex_unlock(&lock);
     printk("\t\tprocessor_container_delete ****************************\n");
@@ -347,16 +385,19 @@ int processor_container_create(struct processor_container_cmd __user *user_cmd)
 {
      int new_cid;
      int new_tid;
-     struct processor_container_cmd userInfo;
      struct task_struct *task=NULL;
+
+     struct processor_container_cmd* userInfo = (struct processor_container_cmd*) kcalloc(1, sizeof(struct processor_container_cmd), GFP_KERNEL);
      mutex_lock(&lock);
      task=current;
      printk("\t\tprocessor_container_create ****************************\n");
-     copy_from_user(&userInfo,user_cmd,sizeof(userInfo));
-     new_cid = (int)userInfo.cid;
+     copy_from_user(userInfo,user_cmd,sizeof(struct processor_container_cmd));
+     new_cid = (int)userInfo->cid;
      new_tid = (int)task->pid;
+     kfree(userInfo);
+     userInfo = NULL;
      printk("creating CID: %d\tTIP: %d\n",new_cid,new_tid);
-     addToContainer(containerList, new_cid, new_tid);
+     addToContainer(new_cid, new_tid);
      // above function unlocks mutex
      // printMap(containerList);
      printk("\t\tprocessor_container_create ****************************\n");
@@ -378,9 +419,9 @@ int processor_container_switch(struct processor_container_cmd __user *user_cmd)
     task=current;
     printk("\t\tprocessor_container_switch ****************************\n");
     curr_tid = (int)task->pid;
-      curr_cid = findContainerForThread(containerList, curr_tid);
+      curr_cid = findContainerForThread(curr_tid);
      if(curr_cid!=-1)
-        addCurrentThreadAtEnd(containerList, curr_cid);
+        addCurrentThreadAtEnd(curr_cid);
         // above function unlocks mutex
      else
      {
@@ -401,6 +442,10 @@ int processor_container_ioctl(struct file *filp, unsigned int cmd,
                               unsigned long arg)
 {
     // printk("in ioctl\n");
+    if(containerList == NULL)
+    {
+      printk("\nContainer List is NULL!!!!!!!!!!!!!!!!!!!!!!\n");
+    }
     switch (cmd)
     {
     case PCONTAINER_IOCTL_CSWITCH:
